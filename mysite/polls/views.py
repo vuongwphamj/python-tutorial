87dad13fbd9004c47c00e1259d6a6dfcf5ce70fb
vuongphamavocado <vuongphamavocado@gmail.com>
2018-01-12 13:06:38 +0700

    Removing hardcoded URLs in templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 # Create your views here.
 # from django.http import HttpResponse
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+# from django.http import Http404
 from .models import Question
 # from django.template import loader
 
@@ -11,11 +12,14 @@ from .models import Question
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
@@ -37,7 +41,7 @@ def vote(request, question_id):
 #     }
 #     return HttpResponse(template.render(context, request))
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.order_by('-pub_date')
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 # Leave the rest of the views (detail, results, vote) unchanged
